24f81060c0b2e00c713706ed1ae53048bf5304a5
Michael <wtfdic@hotmail.com>
2020-11-11 01:56:12 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,12 +1,13 @@
 def turn_count(board)
   turns = 0
-  board.each do |move|
-    if move == "X" || space == "O"
-      turns +1
+  board.each do |space|
+    if space == "X" || space == "O"
+      turns += 1
     end
   end
   return turns
 end
+
 
 def current_player(board)
   turns = turn_count(board)
